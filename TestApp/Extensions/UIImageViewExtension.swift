fe353b417eda5f79ab41770259dc401e7b47bc9b
Igor <izalisky@gmail.com>
2022-11-22 20:06:21 +0200

    --

--- TestApp/Extensions/UIImageViewExtension.swift
+++ TestApp/Extensions/UIImageViewExtension.swift
@@ -11,14 +11,15 @@ import CoreImage
 
 extension UIImageView {
     func downloadImage(fromUrl url: URL) {
-        let request = URLRequest(url: url, cachePolicy: URLRequest.CachePolicy.returnCacheDataElseLoad, timeoutInterval: 60.0)
-        URLSession.shared.dataTask(with: request) { (data, response, error) in
+        let imageCache = NSCache<NSString, UIImage>()
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
             guard
                 let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
                 let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
                 let data = data, error == nil,
                 let image = UIImage(data: data)
                 else { return }
+            imageCache.setObject(image, forKey: url.absoluteURL.absoluteString as NSString)
             DispatchQueue.main.async() { () -> Void in
                 self.image = image
             }
